5721e39e37347aa1b2232cabc5144bd0b1cb58fc
Atul Tripathi <atul31815@gmail.com>
2023-03-01 13:31:04 +0530

    customizedata page

---
+++ pages/customizeData.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import data from '../data/bioData.json'
+
+const customizeData = () => {
+
+  useEffect(() => {
+    let tempData = []
+    let businessData = []
+    let politicalData = []
+    let sportsData = []
+    let otherData = []
+    data.map((item) => {
+      if (item.profession.toLowerCase().includes('business') || item.profession.toLowerCase().includes('entrepreneur') || item.profession.toLowerCase().includes('ceo') || item.profession.toLowerCase().includes('founder') || item.profession.toLowerCase().includes('owner') || item.profession.toLowerCase().includes('manager') || item.profession.toLowerCase().includes('executive')) {
+        businessData.push(item)
+      }
+      else if (item.profession.toLowerCase().includes('politic') || item.profession.toLowerCase().includes('politician') || item.profession.toLowerCase().includes('minister') || item.profession.toLowerCase().includes('president') || item.profession.toLowerCase().includes('prime minister') || item.profession.toLowerCase().includes('senator')) {
+        politicalData.push(item)
+      }
+      else if (item.profession.toLowerCase().includes('cricket') || item.profession.toLowerCase().includes('football') || item.profession.toLowerCase().includes('wrestler') || item.profession.toLowerCase().includes('athlete') || item.profession.toLowerCase().includes('player') || item.profession.toLowerCase().includes('coach') || item.profession.toLowerCase().includes('sportsperson') || item.profession.toLowerCase().includes('sportsman')) {
+        sportsData.push(item)
+      }
+      else {
+        otherData.push(item)
+      }
+      console.log("businessData", businessData)
+      console.log("politicalData", politicalData)
+      console.log("sportsData", sportsData)
+      console.log("otherData", otherData)
+
+      tempData = [...businessData, ...politicalData, ...sportsData, ...otherData]
+
+      tempData.map((item) => {
+        delete item.id
+        delete item?.image_url
+      })
+
+      let uniqueId = 1
+
+      tempData.map((item) => {
+        item.id = uniqueId
+        uniqueId++
+      })
+
+      console.log("tempData", tempData)
+
+    })
+  }, [data])
+
+  return (
+    <div>customizeData</div>
+  )
+}
+
+export default customizeData
