0c7419cbf6ff8f7abe6ee766169683c8f7669849
Atul Tripathi <atul31815@gmail.com>
2023-04-04 00:17:16 +0530

    some ui changes

--- components/Card.js
+++ components/Card.js
@@ -22,7 +22,7 @@ const Card = ({ data, customClasses }) => {
   
 
   return (
-    <div className={`${customClasses} px-2 py-4 md:px-6 lg:px-12 md:py-6 my-2 min-w-full md:min-w-0 md:w-1/2 2xl:w-1/3 flex flex-col items-start rounded-2xl hover:shadow-[-10px_-10px_30px_4px_rgba(139,92,246,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] ease-in-out transition-shadow`}>
+    <div className={`${customClasses} px-4 py-4 md:px-6 lg:px-12 md:py-6 my-2 min-w-full md:min-w-0 md:w-1/2 2xl:w-1/3 flex flex-col items-start rounded-2xl hover:shadow-[-10px_-10px_30px_4px_rgba(139,92,246,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] ease-in-out transition-shadow`}>
       <div className=''>
         <div className='flex items-center justify-between'>
           <img
@@ -36,7 +36,7 @@ const Card = ({ data, customClasses }) => {
           </div>
         </div>
         <div className='mt-3'>
-          <p className="leading-relaxed mb-8 line-clamp-3 text-sm md:text-base">{data?.bio}</p>
+          <p className="leading-relaxed mb-8 line-clamp-4 text-sm md:text-base">{data?.bio}</p>
 
         </div>
       </div>
